2aa505ce1bf12f095340cee00d6332228fab2288
Betty985 <1830265753@qq.com>
2022-05-19 22:40:02 +0800

    feat(面包屑): 动态计算面包屑数据

--- src/components/breadcrumb/index.vue
+++ src/components/breadcrumb/index.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+// 生成数组数据
+const breadcrumbData = ref([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title,
+  )
+  console.log(breadcrumbData.value)
+}
+
+// 监听路由变化时触发
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  { immediate: true },
+)
+</script>
 
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
